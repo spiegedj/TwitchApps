0aaa568322846ae51fa7a4fb243ac758c05ccd7e
spiegedj <spiegedj@uwec.edu>
2020-09-20 00:22:04 -0500

    cycle backgrounds

--- StatusBoard/src/main.js
+++ StatusBoard/src/main.js
@@ -7,3 +7,23 @@ $(document).ready(function () {
     var mainDiv = document.getElementById("main-div");
     ReactDOM.render(React.createElement(StatusBoard_1.StatusBoard), mainDiv);
 });
+const backgrounds = [
+    "back1.jpg",
+    "back2.jpg",
+    //"back3.png",
+    "back4.jpg",
+    "back5.jpg",
+    //"back6.png",
+    //"back7.jpg",
+    "back8.jpg",
+    //"back9.jpg",
+    "back10.jpg",
+    "back11.jpg",
+    "back12.jpg",
+    "back13.jpg",
+];
+let i = 1;
+setInterval(() => {
+    i = (i + 1) % backgrounds.length;
+    document.body.style.backgroundImage = `url(./Images/${backgrounds[i]})`;
+}, 60 * 60 * 1000);
